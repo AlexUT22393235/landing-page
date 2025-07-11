d5a244b6252e2dec78d2687d3e15944efe46b7e4
JesusstdRVM <jesus_std@royalvacationsmexico.com>
2025-07-11 18:01:47 -0500

    Primer corrección de depliegue, eliminando importaciones no usadas. Alex

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./navbar";
 import "./globals.css";
 
